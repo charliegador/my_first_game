3d761de63d8af48db3c5997fb8d4d21c4baefc76
Charlie Gador <charlie.gador@skola.botkyrka.se>
2021-02-22 16:44:48 +0100

    adjustments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Projekt: 
+Project: Don't drop it
+
+- adjust size of circle
+- Give circle acceleration
+- Make it a physical objekt
+- Make it bounce of the top
+- Make it bounce of the platform
+- Game Over function
--- main.lua
+++ main.lua
@@ -3,12 +3,12 @@ y = 100
 platformX = 50
 
 love.draw = function ()
-    love.graphics.circle("line", x, y, 100)
-    love.graphics.rectangle("fill", platformX, 10, 60, 10)
+    love.graphics.circle("fill", x, y, 20)
+    love.graphics.rectangle("fill", platformX, 550, 80, 10)
 end
 
 love.update = function (dt)
     x = x + 100 * dt
-    y = y + 200 * dt
+    y = y + 400 * dt
     platformX = love.mouse.getX()
 end
